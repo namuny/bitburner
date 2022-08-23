10c227bf1648155724732b8de5475e73914a6f1f
namuny <namuny1030@gmail.com>
2022-08-23 21:29:55 +1200

    Write script to get total RAM

--- scripts/util/getTotalRAM.js
+++ scripts/util/getTotalRAM.js
@@ -2,6 +2,8 @@
  * Return total RAM available on all servers with root access
  */
 
+ const NUM_OPEN_PORTS = 5;
+
 /** @param {NS} ns */
 export async function main(ns) {
     var totalRAM = 0;
@@ -21,27 +23,26 @@ export async function main(ns) {
 /** @param {NS} ns */
 async function recurse(ns, target, visited) {
 	if (visited.has(target)) {
-		return;
+		return 0;
 	}
 
 	if (!ns.hasRootAccess(target) && ns.getServerNumPortsRequired(target) > NUM_OPEN_PORTS) {
-		return;
+		return 0;
 	}
 
 	if (ns.getServerRequiredHackingLevel(target) > ns.getHackingLevel()) {
-		return;
+		return 0;
 	}
 
 	visited.add(target);
 	
 	var serverRAM = await ns.getServerMaxRam(target);
-
 	var neighbours = ns.scan(target);
 	for (var neighbour of neighbours) {
 		if (visited.has(neighbour)) {
 			continue;
 		}
-		recurse(ns, neighbour, visited);
+		serverRAM = serverRAM + await recurse(ns, neighbour, visited);
 	}
 
 	return serverRAM;
